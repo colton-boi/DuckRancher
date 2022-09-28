79d482bdfb4232e21a3bab2be471236bb1bf23dc
Colton-boi <zcoltonhirsch@gmail.com>
2022-09-27 21:14:07 -0400

    MongoDB Data Storage

---
+++ src/main/java/me/colton/slimerancher/Data/Mongo.java
@@ -0,0 +1,59 @@
+package me.colton.slimerancher.Data;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+
+import static org.bukkit.Bukkit.getLogger;
+
+public class Mongo {
+
+    private static MongoClient mongoClient;
+
+
+    // Connect to the database
+    private static void connect() {
+
+        if (mongoClient != null) {
+            mongoClient.close();
+        }
+
+        try {
+            ConnectionString connectionString = new ConnectionString("mongodb+srv://dbUser:dbPassword@cluster0.jzrze.mongodb.net/?retryWrites=true&w=majority");
+            MongoClientSettings settings = MongoClientSettings.builder()
+                    .applyConnectionString(connectionString)
+                    .build();
+            mongoClient = MongoClients.create(settings);
+        } catch (Exception e) {
+            getLogger().severe("Could not connect client!");
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Get a database from the current mongoClient
+     *
+     * @param name  the name of the database to find
+     * @return      the database or null if one by the name could not be found
+     */
+    public static MongoDatabase getDatabase(String name) {
+
+        if (mongoClient == null) {
+            connect();
+        }
+
+        try {
+            return mongoClient.getDatabase(name);
+        } catch (Exception e) {
+            getLogger().severe("Could not fetch database!");
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+}
