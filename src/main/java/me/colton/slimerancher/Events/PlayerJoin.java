ba38d2070d39d23cbbaa16e6646ee6a0d63b485a
Colton-boi <zcoltonhirsch@gmail.com>
2022-10-03 13:19:26 -0400

    Pretty bad code, but finished temporary vac pack. Currently pulls in all entities but doesn't do anything once they reach the player.

--- src/main/java/me/colton/slimerancher/Events/PlayerJoin.java
+++ src/main/java/me/colton/slimerancher/Events/PlayerJoin.java
@@ -1,22 +1,20 @@
 package me.colton.slimerancher.Events;
 
-import me.colton.slimerancher.Entities.Enums.SlimeType;
-import me.colton.slimerancher.Entities.Slime;
+import me.colton.slimerancher.Items.VacPack;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.util.UUID;
 
+import static me.colton.slimerancher.SlimeRancher.creatureManager;
+
 public class PlayerJoin implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         UUID player = e.getPlayer().getUniqueId();
-        int i = 0;
-        while (i < 100) {
-            new Slime(e.getPlayer().getLocation(), SlimeType.BOOM, player);
-            i++;
-        }
+        creatureManager.addPlayerToMap(player);
+        new VacPack(e.getPlayer());
     }
 }
