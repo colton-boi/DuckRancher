2507d4b42a02d1a9af5c95c566d6e5b6395f2064
Colton-boi <zcoltonhirsch@gmail.com>
2022-10-11 08:10:33 -0400

    Added spawnCreature and canSpawn functions

--- src/main/java/me/colton/slimerancher/Spawners/SlimeSpawner.java
+++ src/main/java/me/colton/slimerancher/Spawners/SlimeSpawner.java
@@ -1,25 +1,56 @@
 package me.colton.slimerancher.Spawners;
 
+import me.colton.slimerancher.Entities.Creatures.Creature;
 import me.colton.slimerancher.Enums.SpawnerType;
 import org.bukkit.Location;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class SlimeSpawner implements Spawner {
+    private final List<Creature> creatures = new ArrayList<>();
+    private final Location location;
+    private final UUID owner;
+    private double ticksSinceSpawn = 0;
+
+    public SlimeSpawner(Location location, UUID owner) {
+        this.location = location;
+        this.owner = owner;
+    }
+
+    public void tick() {
+        ticksSinceSpawn++;
+        if (!canSpawn()) {
+            return;
+        }
+        if (Math.random() > 0.99) {
+            spawnCreature();
+            ticksSinceSpawn = 0;
+        } else if (ticksSinceSpawn > 50) {
+            spawnCreature();
+            ticksSinceSpawn = 0;
+        }
+    }
 
     @Override
-    public void tick() {
+    public void spawnCreature() {
 
+    }
+
+    @Override
+    public boolean canSpawn() {
+        return false;
     }
 
     @Override
     public Location getLocation() {
-        return null;
+        return location;
     }
 
     @Override
     public UUID getOwner() {
-        return null;
+        return owner;
     }
 
     @Override
--- src/main/java/me/colton/slimerancher/Spawners/Spawner.java
+++ src/main/java/me/colton/slimerancher/Spawners/Spawner.java
@@ -1,16 +1,31 @@
 package me.colton.slimerancher.Spawners;
 
+import me.colton.slimerancher.Entities.Creatures.Creature;
 import me.colton.slimerancher.Enums.SpawnerType;
 import org.bukkit.Location;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public interface Spawner {
+    List<Creature> creatures = new ArrayList<>();
 
     /**
      * Tick the spawner
      */
     void tick();
+
+    /**
+     * Spawn a creature at the spawner
+     */
+    void spawnCreature();
+
+    /**
+     * Check if a creature can be spawned by this spawner
+     * @return      a creature can spawn
+     */
+    boolean canSpawn();
 
     /**
      * Get the location of the spawner
